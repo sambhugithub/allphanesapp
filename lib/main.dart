b8eb24a2f9ff8d0ba2099d1fe3c9062bf6d0dfd0
sambhugithub <sambhumandal348@gmail.com>
2022-08-13 11:33:02 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:allphanes/view/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_sizer/flutter_sizer.dart';
+
+void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp,DeviceOrientation.portraitDown,]
+  );
+  runApp(
+      FlutterSizer(
+        builder: (context, orientation, screenType){
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: "AllPhanes",
+            home: MyApp(),
+          );
+
+  })
+      );
+
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen();
+  }
+}
+
+
+
+
